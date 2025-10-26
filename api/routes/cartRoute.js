dd6aa07b31cb4c7807e3bf19943e8160fb9e2288
Your Name <you@example.com>
2025-10-26 23:26:08 +0530

    four comment twenty six octobar two zero two five

--- api/routes/cartRoute.js
+++ api/routes/cartRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const cartController = require('../controllers/cartController');
-
 router.get('/cart/:userId', cartController.getCart);
 router.post('/cart/:userId/add', cartController.addToCart);
 router.delete('/cart/:userId/remove/:productId', cartController.removeFromCart);
+router.patch('/cart/:userId/update/:productId', cartController.updateQuantity);
 module.exports = router;    
